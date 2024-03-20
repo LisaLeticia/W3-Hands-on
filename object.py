cf0f767a2ea4cbb406bd9aab902112f0478c567e
ellenyp <ivickie621@gmail.com>
2024-03-20 20:52:31 +0800

    Create object.py

---
+++ object.py
@@ -0,0 +1,16 @@
+jstring = "田中さんが車を運転します。"
+print(jstring.split("を")[0])
+#田中さんが車
+print(jstring.split("を")[1])
+#運転します。
+#print(jstring.split("を"))
+print(jstring.split("が")[1])
+#車を運転します。
+print((jstring.split("が")[1]).split("を")[0])
+#車
+
+
+    
+        
+        
+
