00d174f19c0040b73dc62e6a5fb8695f5cd92458
ellenyp <ivickie621@gmail.com>
2024-03-21 19:35:54 +0800

    0321 update

--- object.py
+++ object.py
@@ -1,13 +1,34 @@
-jstring = "田中さんが車を運転します。"
-print(jstring.split("を")[0])
-#田中さんが車
-print(jstring.split("を")[1])
-#運転します。
-#print(jstring.split("を"))
-print(jstring.split("が")[1])
-#車を運転します。
-print((jstring.split("が")[1]).split("を")[0])
-#車
+
+f = open("inputSTR.txt", "r")  #讀取data(要和code放在同一個資料夾)
+inputSTR = data.read().replace('\n', " ")  #讓input等於讀取進來的資料，將分行用空格取代
+#這裡我們應該可以再討論要不要調整，這是Emily他們那組的作法(而且現在跑不過去QQ)
+whole_list = inputSTR.split('。 ')  #幫切好的資料取名為whole_list
+
+def extractObject(inputLIST):
+     
+    jstring = "田中さんが車を運転します。" #測試句子一
+    a = "先生が生徒に教えます。"          #測試句子二
+    
+    print(jstring.split("が")[0])                   #subject
+    print(jstring.split("を")[1])                   #verb
+    print((jstring.split("が")[1]).split("を")[0])  #object
+    #print(a.split("に")[1])
+    
+    jstring = "田中さんが車を運転します。"
+    if "を" in jstring:
+        print((jstring.split("が")[1]).split("を")[0])
+    if "に" in a:
+        print((a.split("が")[1]).split("に")[0])
+        
+
+    
+for i in whole_list:
+    jstring = i
+    #subject = jstring.split("が")[0]
+    #verb = jstring.split("を")[1]
+    object =extractObject(jstring)
+
+
 
 
     
